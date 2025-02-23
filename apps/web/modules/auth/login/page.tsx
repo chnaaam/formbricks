751521d132fff05f1f9a55064509cb30a9391af4
chnaaam <a71335976@gmail.com>
2025-02-23 22:38:20 +0900

    fix: update login UI

--- apps/web/modules/auth/login/page.tsx
+++ apps/web/modules/auth/login/page.tsx
@@ -1,5 +1,4 @@
 import { FormWrapper } from "@/modules/auth/components/form-wrapper";
-import { Testimonial } from "@/modules/auth/components/testimonial";
 import { getIsMultiOrgEnabled, getIsSSOEnabled } from "@/modules/ee/license-check/lib/utils";
 import { Metadata } from "next";
 import {
@@ -22,26 +21,21 @@ export const metadata: Metadata = {
 export const LoginPage = async () => {
   const [isMultiOrgEnabled, isSSOEnabled] = await Promise.all([getIsMultiOrgEnabled(), getIsSSOEnabled()]);
   return (
-    <div className="grid min-h-screen w-full bg-gradient-to-tr from-slate-100 to-slate-50 lg:grid-cols-5">
-      <div className="col-span-2 hidden lg:flex">
-        <Testimonial />
-      </div>
-      <div className="col-span-3 flex flex-col items-center justify-center">
-        <FormWrapper>
-          <LoginForm
-            emailAuthEnabled={EMAIL_AUTH_ENABLED}
-            publicSignUpEnabled={SIGNUP_ENABLED}
-            passwordResetEnabled={!PASSWORD_RESET_DISABLED}
-            googleOAuthEnabled={GOOGLE_OAUTH_ENABLED}
-            githubOAuthEnabled={GITHUB_OAUTH_ENABLED}
-            azureOAuthEnabled={AZURE_OAUTH_ENABLED}
-            oidcOAuthEnabled={OIDC_OAUTH_ENABLED}
-            oidcDisplayName={OIDC_DISPLAY_NAME}
-            isMultiOrgEnabled={isMultiOrgEnabled}
-            isSSOEnabled={isSSOEnabled}
-          />
-        </FormWrapper>
-      </div>
+    <div className="flex min-h-screen w-full items-center justify-center bg-gradient-to-tr from-slate-100 to-slate-50">
+      <FormWrapper>
+        <LoginForm
+          emailAuthEnabled={EMAIL_AUTH_ENABLED}
+          publicSignUpEnabled={SIGNUP_ENABLED}
+          passwordResetEnabled={!PASSWORD_RESET_DISABLED}
+          googleOAuthEnabled={GOOGLE_OAUTH_ENABLED}
+          githubOAuthEnabled={GITHUB_OAUTH_ENABLED}
+          azureOAuthEnabled={AZURE_OAUTH_ENABLED}
+          oidcOAuthEnabled={OIDC_OAUTH_ENABLED}
+          oidcDisplayName={OIDC_DISPLAY_NAME}
+          isMultiOrgEnabled={isMultiOrgEnabled}
+          isSSOEnabled={isSSOEnabled}
+        />
+      </FormWrapper>
     </div>
   );
 };
